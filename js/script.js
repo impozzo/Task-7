7dc86b78d323779f93720c9ce5aec45aa5a76e81
Michael Weisinger <impozzo@verizon.net>
2020-11-15 14:33:41 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,52 @@
+
+// write a function that takes the input string and reverses it
+const stringToReverse = "This"
+let x = stringToReverse.reverseThisString;
+reverseThisString(stringToReverse);
+console.log(x);
+function reverseThisString(ToReverse){
+  const x = ToReverse.reverseThisString; 
+    console.log(ToReverse);
+    console.log(x);
+
+} 
+reverseThisString(stringToReverse);
+console.log('test') ; 
+
+
+
+//write a function that takes the input string and switches all uppercase characters to lowercase and lowercase charcaters to uppercase
+function swapCase(string) {
+ 
+} 
+
+//convert array of numbers from farenheit to celcius
+function toCelcius(array){
+
+}
+
+
+//write a function that takes an input array and returns an array of booleans (>=75) or fail (<75)
+function passOrFail(array){
+
+}
+
+
+//write code that loops through the two variables returns an array ['2 is zwei', '3 is drei', '4 is vier', '5 is fünf', '6 is sechs']
+function germanNumbers(){
+    const cardinalNumbers = [2,3,4,5,6];
+    const germanNumbers = ['zwei', 'drei', 'vier', 'fünf', 'sechs'];
+
+}
+
+
+// write code that returns an array of ONLY prime numbers that are in the array numbers
+function returnPrimeNumbers(){
+    const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+
+}
+
+// Write a function that loops through and console.log's the numbers from 1 to 100, except multiples of three, log (without quotes) "CSC225 RULES" instead of the number, for the multiples of five, log (without quotes) "I LOVE JAVASCRIPT". For numbers which are multiples of both three and five, log (without quotes) "CSC225 RULES I LOVE JAVASCRIPT" 
+function csc225Rules(){ 
+
+}
